7d6b31096fcdf6c09eb22db5cbb49e61b76ee6d1
Chidiogo <chidiogoezeh.me@gmail.com>
2026-02-03 16:27:44 +0100

    updated taskRoutes.js to enable users to be able to add audio as alarm and clear finished tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,25 @@
 import express from 'express';
-import { getTasks, addTask, updateTask } from '../controllers/taskController.js';
+import { 
+    getTasks, 
+    addTask, 
+    updateTask, 
+    deleteTask 
+} from '../controllers/taskController.js';
 import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.use(protect); // All task routes protected
-router.route('/').get(getTasks).post(addTask);
-router.route('/:id').put(updateTask);
+// Protect all routes after this middleware
+router.use(protect); 
+
+// Chain GET and POST for the root path
+router.route('/')
+    .get(getTasks)
+    .post(addTask);
+
+// Chain PUT and DELETE for the specific task ID
+router.route('/:id')
+    .put(updateTask)
+    .delete(deleteTask);
 
 export default router;
